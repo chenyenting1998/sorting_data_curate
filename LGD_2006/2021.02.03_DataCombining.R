0f9177d6fde2c2598a7550d13c5378b6d13766cd
chenyenting1998 <tumhark@gmail.com>
2021-08-27 16:14:23 +0800

    stylized

--- LGD_2006/2021.02.03_DataCombining.R
+++ LGD_2006/2021.02.03_DataCombining.R
@@ -7,16 +7,16 @@ library(tibble)
 setwd("C:\\Users\\tumha\\Desktop\\Lab Work\\Data\\Sorting data\\LGD_2006")
 # 100 = 1 cm = 10 mm
 size <- read_xlsx("20210203_LGD-2006_RawTypingFile.xlsx", sheet = 1) %>%
-  mutate(L = L*0.1, W = W*0.1) %>%
+  mutate(L = L * 0.1, W = W * 0.1) %>%
   mutate(Habitat = "Shelf", Deployment = 1, Section = "0-10") %>%
-  select(Cruise, Habitat, Station, Deployment, Tube, Section, Taxon, Family, Genus, Condition, Type, L, W, a,b, Size, Note)
+  select(Cruise, Habitat, Station, Deployment, Tube, Section, Taxon, Family, Genus, Condition, Type, L, W, a, b, Size, Note)
 poly <- read_xlsx("20210203_LGD-2006_RawTypingFile.xlsx", sheet = 2)
 
 write_xlsx(list(macro = size, polychaete = poly), "LGD-2006_macro_size.xlsx") # lack polychaete infromation
 
 
-#check<- size %>% select(Taxon, Condition, Note) %>% distinct %>% arrange(Taxon)
-#check
+# check<- size %>% select(Taxon, Condition, Note) %>% distinct %>% arrange(Taxon)
+# check
 
 ###########################################
 # Revise ZHJ3 Ophiuroids Note information #
--- LGD_2006/2021.07.29_DataCombining.R
+++ LGD_2006/2021.07.29_DataCombining.R
@@ -1,82 +1,65 @@
-setwd("C:\\Users\\tumha\\Desktop\\Lab Work\\Data\\Sorting data\\LGD_2006")
-
-library(readxl)
-library(writexl)
-library(dplyr)
-
+pacman::p_load(
+  readxl,
+  writexl,
+  dplyr
+)
 
 # Macrofauna --------------------------------------------------------------
 # stations include all the stations measured previously (the ophiuroids were measured arbitrarily)
-raw_20210203 <- read_xlsx("20210203_LGD-2006_RawTypingFile.xlsx")
+raw_20210203 <- read_xlsx("LGD_2006/20210203_LGD-2006_RawTypingFile.xlsx")
 # stations include P4 N1 and such, the ophiuroids were measured with the new way
-raw_20210729 <- read_xlsx("20210729_LGD-2006_RawTypingFile.xlsx")
+raw_20210729 <- read_xlsx("LGD_2006/20210729_LGD-2006_RawTypingFile.xlsx")
 
-#View(raw_20210203)
-#View(raw_20210729)
-
-# oph_rem of the raw_20210203
-oph_re <- read_xlsx("LGD-2006_oph_rem.xlsx")
+# View(raw_20210203)
+# View(raw_20210729)
 
 # reform raw_20210203
-raw_20210203_add_oph <- 
+raw_20210203_add_oph <-
   raw_20210203 %>%
   filter(Taxon != "Ophiuroidea") %>%
   select(-...18) %>%
   full_join(oph_re) %>%
-  mutate(L = 0.1 * L, W = 0.1 * W) %>% 
+  mutate(L = 0.1 * L, W = 0.1 * W) %>%
   mutate(Habitat = "Shelf", Deployment = 1, Section = "0-10")
 
+# the section length in P4 and N1 can be obtained from Dr. YSL in NSYSU
 raw_20210729_modified <-
   raw_20210729 %>%
-  mutate(L = 0.1 * L, W = 0.1 * W) %>% 
-  mutate(Habitat = "Shelf")   
-  
+  mutate(L = 0.1 * L, W = 0.1 * W) %>%
+  mutate(Habitat = "Shelf")
+
+# ophuiroid ---------------------------------------------------------------
+# oph_rem of the raw_20210203
+oph_re <- read_xlsx("LGD_2006/LGD-2006_oph_rem.xlsx")
 
 macro <- full_join(raw_20210203_add_oph, raw_20210729_modified)
 
+# Change specific values --------------------------------------------------
 # changing one value
-disk_like_bryo <- 
-  macro %>% 
-  filter(Note == "disc-like colony") %>% 
-  mutate(Note = "Colony")
+macro[!is.na(macro$Note) & macro$Note == "disc-like colony", ]$Note <- "Colony"
 
-macro <-
-  macro %>%
-  filter(!Note %in% "disc-like colony") %>%
-  full_join(disk_like_bryo)
+# add value "Polyp" to entoprocta
+macro[macro$Taxon == "Entoprocta", ]$Note <- "Polyp"
 
-
-# add value polyp to entoprocta
-macro <-
-  macro %>%
-  mutate(Note = if_else(Taxon == "Entoprocta", "Polyp", NA_character_))
+# change arachnida to Acari
+macro[macro$Taxon == "Arachnida", ]$Taxon <- "Acari"
 
 
 # polychaete --------------------------------------------------------------
-poly_re <- read_excel("LGD-2006_poly_rem.xlsx")
-
-#View(poly_re)
-#View(macro)
+poly_re <- read_excel("LGD_2006/LGD-2006_poly_rem.xlsx")
 
 # add information
 poly_re_modified <-
-  poly_re %>% 
+  poly_re %>%
   mutate(Habitat = "Shelf") %>%
-  mutate(L = L * 0.1) %>%
-  mutate(W = W * 0.1) %>%
-  mutate(Section = if_else(Station %in% c("P4", "N1"), NA_character_, "0-10")) 
-#`if_else` is so rigid that the true and false condition should be in the same class
+  mutate(L = L * 0.1, W = W * 0.1) %>%
+  mutate(Section = if_else(Station %in% c("P4", "N1"), NA_character_, "0-10"))
 
-poly_re_modified$Condition <- poly_re_modified$Condition %>% toupper
+# Upper case the polychaete condition
+poly_re_modified$Condition <- poly_re_modified$Condition %>% toupper()
 
-# the section length in P4 and N1 can be obtained from Dr. YSL in NSYSU
 
-# change arachnida to Acari
-macro[macro$Taxon == "Arachnida",]$Taxon <- "Acari"  
 
-# write excel
+# write excel -------------------------------------------------------------
 full_join(macro, poly_re_modified) %>%
   write_xlsx("LGD-2006_macro_size_final.xlsx")
-
-
-
